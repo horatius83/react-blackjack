3b1cd738de4eb74d5d2864bd60bb54473b9344cb
M Monster <horatius83@gmail.com>
2021-02-09 17:56:07 -0600

    Added game test stub

---
+++ typescript-blackjack-app/src/models/game.test.ts
@@ -0,0 +1,10 @@
+
+describe('getRoundsummary', () => {
+    test('pushes greater than 21 do not count', () => {
+    //expect(linkElement).toBeInTheDocument();
+        const game = {
+
+        };
+        expect(true).toBe(true);
+    });
+})
